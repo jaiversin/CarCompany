7dcb8afca2a077704b23019058165e93757ac369
jaiversin <jaiversin@gmail.com>
2018-04-05 21:07:29 -0500

    Initial commit

---
+++ CarCompany/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarCompany
+//
+//  Created by Jhon on 4/5/18.
+//  Copyright © 2018 Jhon Jaiver López. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CarCompanyTests/CarCompanyTests.swift
@@ -0,0 +1,23 @@
+//
+//  CarCompanyTests.swift
+//  CarCompanyTests
+//
+//  Created by Jhon on 4/5/18.
+//  Copyright © 2018 Jhon Jaiver López. All rights reserved.
+//
+
+import XCTest
+@testable import CarCompany
+
+class CarCompanyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+}
