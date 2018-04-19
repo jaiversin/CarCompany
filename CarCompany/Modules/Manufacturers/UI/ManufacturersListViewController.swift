7fa65b8d16b4e5a3f11ce844d7f1777148ec270b
jaiversin <jaiversin@gmail.com>
2018-04-19 00:22:26 -0500

    [Manufacturers] View controller table view and cells customisation

--- CarCompany/Modules/Manufacturers/UI/ManufacturersListViewController.swift
+++ CarCompany/Modules/Manufacturers/UI/ManufacturersListViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class ManufacturersListViewController: UIViewController {
     
     fileprivate let viewModel = ManufacturersListViewModel()
     
@@ -16,30 +16,44 @@ final class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Manufacturers"
+        
         self.manufacturersTableView.delegate = self
         self.manufacturersTableView.dataSource = self
         viewModel.onListDidChange = { [weak self] in
             self?.manufacturersTableView.reloadData()
         }
         viewModel.page = 0
-        
     }
 }
 
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
+// MARK: - UITableViewDataSource
+
+extension ManufacturersListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return (viewModel.manufacturers ?? []).count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        let cellType = (indexPath.row % 2 == 0 ? "even": "odd" )
+        let cell = manufacturersTableView.dequeueReusableCell(withIdentifier: cellType)!
+        
         cell.textLabel?.text = viewModel.manufacturers?[indexPath.row].name
         return cell
     }
     
+}
+
+// MARK: - UITableViewDelegate
+
+extension ManufacturersListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         if indexPath.row >= (viewModel.manufacturers?.count ?? 0) - 1 {
             viewModel.incrementPage()
         }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
